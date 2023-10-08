2a40149e3d8609ef3176dd607876fe80aac129c1
lequangkhai06 <khaidev.com@gmail.com>
2023-10-08 22:33:28 +0700

    Thêm tính năng: tính điểm tốt nghiệp thpt, gdtx

--- loading.py
+++
@@ -1,9 +0,0 @@
-import customtkinter as ctk
-app=ctk.CTk()
-textbox = ctk.CTkTextbox(app)
-
-textbox.insert("0.0", "new text to insert")  # insert at line 0 character 0
-# get text from line 0 character 0 till the end
-text = textbox.get("0.0", "end")
-textbox.delete("0.0", "end")  # delete all text
-textbox.configure(state="disabled")  # configure textbox to be read-only
